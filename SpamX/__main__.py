672f0a42c2f4b60d99717ca560ad57f4b1fdfb1a
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-10-18 23:25:57 +0530

    Update __main__.py

--- SpamX/__main__.py
+++ SpamX/__main__.py
@@ -11,86 +11,111 @@ from . import (RiZoeL, RiZoeL2, RiZoeL3, RiZoeL4, RiZoeL5,
                 RiZoeL11, RiZoeL12, RiZoeL13, RiZoeL14, RiZoeL15, 
                 RiZoeL16, RiZoeL17, RiZoeL18, RiZoeL19, RiZoeL20, hl)
 
-if RiZoeL:
+from . import (CLIENT, CLIENT2, CLIENT3, CLIENT4, CLIENT5, 
+                CLIENT6, CLIENT7, CLIENT8, CLIENT9, CLIENT10, 
+                CLIENT11, CLIENT12, CLIENT13, CLIENT14, CLIENT15, 
+                CLIENT16, CLIENT17, CLIENT18, CLIENT19, CLIENT20)
+
+if CLIENT:
     RiZoeL.start()
-    RiZoeL.join_chat("RiZoeLX")
-    RiZoeL.join_chat("DNHxHELL")
-if RiZoeL2:
+    if not ":" in CLIENT:
+       RiZoeL.join_chat("RiZoeLX")
+       RiZoeL.join_chat("DNHxHELL")
+if CLIENT2:
     RiZoeL2.start()
-    RiZoeL2.join_chat("RiZoeLX")
-    RiZoeL2.join_chat("DNHxHELL")
-if RiZoeL3:
+    if not ":" in CLIENT2:
+      RiZoeL2.join_chat("RiZoeLX")
+      RiZoeL2.join_chat("DNHxHELL")
+if CLIENT3:
     RiZoeL3.start()
-    RiZoeL3.join_chat("RiZoeLX")
-    RiZoeL3.join_chat("DNHxHELL")
-if RiZoeL4:
+    if not ":" in CLIENT3:
+       RiZoeL3.join_chat("RiZoeLX")
+       RiZoeL3.join_chat("DNHxHELL")
+if CLIENT4:
     RiZoeL4.start()
-    RiZoeL4.join_chat("RiZoeLX")
-    RiZoeL4.join_chat("DNHxHELL")
-if RiZoeL5:
+    if not ":" in CLIENT4:
+       RiZoeL4.join_chat("RiZoeLX")
+       RiZoeL4.join_chat("DNHxHELL")
+if CLIENT5:
     RiZoeL5.start()
-    RiZoeL5.join_chat("RiZoeLX")
-    RiZoeL5.join_chat("DNHxHELL")
-if RiZoeL6:
+    if not ":" in CLIENT5:
+       RiZoeL5.join_chat("RiZoeLX")
+       RiZoeL5.join_chat("DNHxHELL")
+if CLIENT6:
     RiZoeL6.start()
-    RiZoeL6.join_chat("RiZoeLX")
-    RiZoeL6.join_chat("DNHxHELL")
-if RiZoeL7:
+    if not ":" in CLIENT6:
+       RiZoeL6.join_chat("RiZoeLX")
+       RiZoeL6.join_chat("DNHxHELL")
+if CLIENT7:
     RiZoeL7.start()
-    RiZoeL7.join_chat("RiZoeLX")
-    RiZoeL7.join_chat("DNHxHELL")
-if RiZoeL8:
+    if not ":" in CLIENT7:
+       RiZoeL7.join_chat("RiZoeLX")
+       RiZoeL7.join_chat("DNHxHELL")
+if CLIENT8:
     RiZoeL8.start()
-    RiZoeL8.join_chat("RiZoeLX")
-    RiZoeL8.join_chat("DNHxHELL")
-if RiZoeL9:
+    if not ":" in CLIENT8:
+       RiZoeL8.join_chat("RiZoeLX")
+       RiZoeL8.join_chat("DNHxHELL")
+if CLIENT9:
     RiZoeL9.start()
-    RiZoeL9.join_chat("RiZoeLX")
-    RiZoeL9.join_chat("DNHxHELL")
-if RiZoeL10:
+    if not ":" in CLIENT9:
+       RiZoeL9.join_chat("RiZoeLX")
+       RiZoeL9.join_chat("DNHxHELL")
+if CLIENT10:
     RiZoeL10.start()
-    RiZoeL10.join_chat("RiZoeLX")
-    RiZoeL10.join_chat("DNHxHELL")
-if RiZoeL11:
+    if not ":" in CLIENT10:
+       RiZoeL10.join_chat("RiZoeLX")
+       RiZoeL10.join_chat("DNHxHELL")
+if CLIENT11:
     RiZoeL11.start()
-    RiZoeL11.join_chat("RiZoeLX")
-    RiZoeL11.join_chat("DNHxHELL")
-if RiZoeL12:
+    if not ":" in CLIENT11:
+       RiZoeL11.join_chat("RiZoeLX")
+       RiZoeL11.join_chat("DNHxHELL")
+if CLIENT12:
     RiZoeL12.start()
-    RiZoeL12.join_chat("RiZoeLX")
-    RiZoeL12.join_chat("DNHxHELL")
-if RiZoeL13:
+    if not ":" in CLIENT12:
+       RiZoeL12.join_chat("RiZoeLX")
+       RiZoeL12.join_chat("DNHxHELL")
+if CLIENT13:
     RiZoeL13.start()
-    RiZoeL13.join_chat("RiZoeLX")
-    RiZoeL13.join_chat("DNHxHELL")
-if RiZoeL14:
+    if not ":" in CLIENT13:
+       RiZoeL13.join_chat("RiZoeLX")
+       RiZoeL13.join_chat("DNHxHELL")
+if CLIENT14:
     RiZoeL14.start()
-    RiZoeL14.join_chat("RiZoeLX")
-    RiZoeL14.join_chat("DNHxHELL")
-if RiZoeL15:
+    if not ":" in CLIENT14:
+       RiZoeL14.join_chat("RiZoeLX")
+       RiZoeL14.join_chat("DNHxHELL")
+if CLIENT15:
     RiZoeL15.start()
-    RiZoeL15.join_chat("RiZoeLX")
-    RiZoeL15.join_chat("DNHxHELL")
-if RiZoeL16:
+    if not ":" in CLIENT15:
+       RiZoeL15.join_chat("RiZoeLX")
+       RiZoeL15.join_chat("DNHxHELL")
+if CLIENT16:
     RiZoeL16.start()
-    RiZoeL16.join_chat("RiZoeLX")
-    RiZoeL16.join_chat("DNHxHELL")
-if RiZoeL17:
+    if not ":" in CLIENT16:
+       RiZoeL16.join_chat("RiZoeLX")
+       RiZoeL16.join_chat("DNHxHELL")
+if CLIENT17:
     RiZoeL17.start()
-    RiZoeL17.join_chat("RiZoeLX")
-    RiZoeL17.join_chat("DNHxHELL")
-if RiZoeL18:
+    if not ":" in CLIENT17:
+       RiZoeL17.join_chat("RiZoeLX")
+       RiZoeL17.join_chat("DNHxHELL")
+if CLIENT18:
     RiZoeL18.start()
-    RiZoeL18.join_chat("RiZoeLX")
-    RiZoeL18.join_chat("DNHxHELL")
-if RiZoeL19:
+    if not ":" in CLIENT18:
+       RiZoeL18.join_chat("RiZoeLX")
+       RiZoeL18.join_chat("DNHxHELL")
+if CLIENT19:
     RiZoeL19.start()
-    RiZoeL19.join_chat("RiZoeLX")
-    RiZoeL19.join_chat("DNHxHELL")
-if RiZoeL20:
+    if not ":" in CLIENT18:
+       RiZoeL19.join_chat("RiZoeLX")
+       RiZoeL19.join_chat("DNHxHELL")
+if CLIENT20:
     RiZoeL20.start()
-    RiZoeL20.join_chat("RiZoeLX")
-    RiZoeL20.join_chat("DNHxHELL")
+    if not ":" in CLIENT20:
+       RiZoeL20.join_chat("RiZoeLX")
+       RiZoeL20.join_chat("DNHxHELL")
 
 print("Your Spam Bots Successfully Deployed ✅")
 print("Visit -! @RiZoeLX")
