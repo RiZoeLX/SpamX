1dde781812becd0be84e8d1d0f9e833cfd36005a
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-11-06 11:27:46 +0530

    Update Spam.py

--- SpamX/module/Spam.py
+++ SpamX/module/Spam.py
@@ -30,7 +30,7 @@ async def delayspam(xspam: Client, e: Message):
             return await e.reply_text("**Sorry !! i Can't Spam Here.**")
        msg = str(Rizoelop[1])
        if re.search(Owners.lower(), msg.lower()):
-            return await e.reply_text(usage)("**Sorry !!** I can't Spam On @RiZoeLX's owner")
+            return await e.reply_text("**Sorry !!** I can't Spam On @RiZoeLX's owner")
        sleeptime = float(Rizoel[0])
        if e.reply_to_message:
           reply_to_id = e.reply_to_message.message_id
