2a3b2dbe635c8b680c62bfa89155177a2a9a3dab
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-11-09 10:18:12 +0530

    Update Message_all.py

--- SpamX/module/Message_all.py
+++ SpamX/module/Message_all.py
@@ -14,19 +14,18 @@ from SpamX.data import *
 async def msgall(xspam: Client, e: Message): 
       fuk = e.text[7:]
       if fuk:
-          chat_id = e.chat.id
-          user_id = e.from_user.id
-          if chat_id == user_id:
+          chat = e.chat.id
+          user = e.from_user.id
+          if chat == user:
               """ Can't Use this Cmd in PM """
               return await e.reply_text("Use This Cmd in group")
           msg = str(fuk)
           if int(e.chat.id) in GROUP:
                return await e.reply_text("**Sorry !! You can't use this cmd in this Group-!**")
           await e.reply_text("__Sending Message to all group members__")
-          lol = await xspam.send_message(user_id, "sending Message to all group Members !")
           ok = 0
           f = 0
-          async for x in xspam.get_chat_members(chat_id):
+          async for x in xspam.get_chat_members(chat):
              c = x.user.id
              try:
                 ok += 1
@@ -35,7 +34,7 @@ async def msgall(xspam: Client, e: Message):
              except Exception as a:
                 f += 1
                 print(a)
-          return await lol.edit_text(f"Messaged all group members! \n\nsent to `{ok}` users \nfailed: `{f}`")
+          return await xspam.send_message(user, f"Messaged all group members! \n\nsent to `{ok}` users \nfailed: `{f}`")
 
       else:
            await e.reply_text(f"**Wrong usage -!** \n\n Syntax: \n {HNDLR}msgall (Your message)")
