dd32baa4ce1632d4dacab203c98a7eb9fa5e8f2f
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-12-29 16:56:44 +0530

    Create sh.py

---
+++ sh.py
@@ -0,0 +1,32 @@
+import os, sys
+from resources.startup import *
+ 
+os.system("clear")
+
+# ------ main ------ #
+start = input(f"{ask}What to fill vars ? if yes type Y/yes else press enter: ")
+if start in ['Y', 'y', 'Yes', 'YES', 'yes']:
+  if os.path.exists(".env"):
+    x = open(".env", "r")
+    check = x.read()
+    lines = check.splitlines()
+    x.close()
+    if not len(lines) == 29:
+        os.system("rm -rf .env")
+        y = open(".env", "w")
+        y.write(evn_vars)
+        y.close()
+        os.system("clear")
+        SpamX_Setup()
+    else:
+        os.system("clear")
+        SpamX_Setup()
+  elif not os.path.exists(".env"):
+    y = open(".env", "w")
+    y.write(evn_vars)
+    y.close()
+    os.system("clear")
+    SpamX_Setup()
+else:
+  clear()
+  os.system("python3 -m SpamX")
