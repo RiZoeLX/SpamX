68b304715b7bb51e7c05d2c1960d4cc40ae23264
S L A D E <sladexsarcasm@gmail.com>
2023-01-10 14:10:26 +0530

    Update alive.py

--- SpamX/modules/for_id/alive.py
+++ SpamX/modules/for_id/alive.py
@@ -47,7 +47,7 @@ async def SpamX_(SpamX: Client, message: Message):
     Mai = await SpamX.get_me()
     chat = message.chat
     info = await message.reply_text("processing......")
-    spamx_reply = "**__SpamX by RiZoeLX;__** \n\n"
+    spamx_reply = "**__SpamX by 𝖠 𝖭 𝖴 𝖩 • 死鬼;__** \n\n"
     spamx_reply += "<=======================> \n"
 
     try:
@@ -106,8 +106,8 @@ async def SpamX_(SpamX: Client, message: Message):
        spamx_reply += f"**Total Sudos:** `{users_db.sudo_count()}` \n"
        spamx_reply += f"**Total Gbanned users:** `{gban_db.gban_count()}` \n"
     spamx_reply += "<=======================> \n\n"
-    spamx_reply += "**Source Code:** [GitHub 🐈‍⬛](https://github.com/RiZoeLX/SpamX) \n\n"
-    spamx_reply += "**© @RiZoeLX** || Support: @DNHxHELL"
+    spamx_reply += "**Source Code:** [GitHub 🐈‍⬛](https://github.com/spryslade/SpamX) \n\n"
+    spamx_reply += "**© @thelord_xD** || Support: @AstorSupport"
     
     try:
        await info.edit_text(spamx_reply, disable_web_page_preview=True)
