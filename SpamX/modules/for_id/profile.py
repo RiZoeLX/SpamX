41a89a42f170c9aea51a5a49fda72288b993e909
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-28 21:52:45 +0530

    Update profile.py

--- SpamX/modules/for_id/profile.py
+++ SpamX/modules/for_id/profile.py
@@ -32,32 +32,39 @@ etc_bio = "ᴜsᴇʀ ᴏғ ʀɪᴢᴏᴇʟ x"
 @Client.on_message(filters.user(Owner) & filters.command(["setname"], prefixes=handler))
 @Client.on_message(filters.me & filters.command(["setname"], prefixes=handler))
 async def setname(SpamX: Client, e: Message): 
-      Rizoel = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 1)
-      if len(Rizoel) == 1:
-          name = str(Rizoel[0])
-          try:
-            await SpamX.update_profile(first_name=name, bio=etc_bio)
-            await e.reply_text(f"**Profile Name Changed Successfully !!** \n\n **New Name:** {name}")
-          except Exception as ex:
-            await e.reply_text(f"**Error !!** \n\n {ex}")
-            print(ex)
-      else:
-          await e.reply_text(f"Wrong usage! \n syntax: {handler}setname (name)")
-
+     Rizoel = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 1)
+     if e.reply_to_message:
+        name = e.reply_to_message.text.markdown
+     elif Rizoel:
+        name = str(Rizoel[0])
+     else:
+        await e.reply_text(f"Wrong usage! \n syntax: {handler}setname (name)")
+        return
+     try:
+        await SpamX.update_profile(first_name=name, bio=etc_bio)
+        await e.reply_text(f"**Profile Name Changed Successfully !!** \n\n **New Name:** {name}")
+     except Exception as ex:
+        await e.reply_text(f"**Error !!** \n\n {ex}")
+        print(ex)
+     
 @Client.on_message(filters.user(Owner) & filters.command(["setbio"], prefixes=handler))
 @Client.on_message(filters.me & filters.command(["setbio"], prefixes=handler))
 async def setbio(SpamX: Client, e: Message):
       Rizoel = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 1)
-      if len(Rizoel) == 1:
-          xd = str(Rizoel[0])
-          ok = await SpamX.get_me()
-          nam = ok.first_name
-          nam2 = ok.last_name
-          try:
-             await SpamX.update_profile(first_name=nam, last_name=nam2, bio=xd)
-             await e.reply_text(f"**Profile Bio Changed Successfully !** \n\n **New Bio**: {xd}")
-          except Exception as ex:
-            await e.reply_text(f"**Error !!** \n\n {ex}")
-            print(ex)
+      if e.reply_to_message:
+        xd = e.reply_to_message.text.markdown
+      elif Rizoel:
+        xd = str(Rizoel[0])
       else:
-          await e.reply_text(f"Wrong usage! \n syntax: {handler}setbio (bio message)")
+        await e.reply_text(f"Wrong usage! \n syntax: {handler}setbio (bio)")
+        return
+      ok = await SpamX.get_me()
+      nam = ok.first_name
+      nam2 = ok.last_name
+      try:
+         await SpamX.update_profile(first_name=nam, last_name=nam2, bio=xd)
+         await e.reply_text(f"**Profile Bio Changed Successfully !** \n\n **New Bio**: {xd}")
+       except Exception as ex:
+         await e.reply_text(f"**Error !!** \n\n {ex}")
+         print(ex)
+      
