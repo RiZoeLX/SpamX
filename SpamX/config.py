5d8b85a08bd907b5411ec36a5386e778d5bf4fc3
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2024-02-22 21:45:26 +0530

    yeah - config.py

--- SpamX/config.py
+++ SpamX/config.py
@@ -1,5 +1,9 @@
 from os import getenv as env
 from SpamX.functions.logger import LOGS
+from dotenv import load_dotenv
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
 
 API_ID = int(env("API_ID", None))
 if API_ID is None:
